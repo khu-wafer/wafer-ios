d1da8999b8cf900ec2f5c0b5f843b96ea03680bf
daminoworld <ekadls757@gmail.com>
2023-04-16 17:38:14 +0900

    Add: Add initial project files

---
+++ khu-wafer/khu-wafer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  khu-wafer
+//
+//  Created by 김담인 on 2023/04/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
