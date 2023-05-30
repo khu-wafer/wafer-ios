3e81fe05a7ccb1fb9e50f50fc46e7bb18d35e71a
daminoworld <ekadls757@gmail.com>
2023-05-30 22:36:15 +0900

    Add: Add HomeVC, ControlVC file

---
+++ khu-wafer/khu-wafer/Control/ControlVC.swift
@@ -0,0 +1,29 @@
+//
+//  ControlVC.swift
+//  khu-wafer
+//
+//  Created by 김담인 on 2023/05/30.
+//
+
+import UIKit
+
+class ControlVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
