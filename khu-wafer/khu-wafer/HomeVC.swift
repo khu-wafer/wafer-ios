c6ebd6cdd43b134acb7febd51731c795e3d9ac69
daminoworld <ekadls757@gmail.com>
2023-05-30 23:13:47 +0900

    Feat: Set Home View UI

--- khu-wafer/khu-wafer/HomeVC.swift
+++ khu-wafer/khu-wafer/HomeVC.swift
@@ -6,24 +6,71 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeVC: UIViewController {
+    
+    private let headerLabel: UILabel = {
+       let lb = UILabel()
+        lb.text = "Khu WareHouse"
+        lb.textColor = .black
+        lb.font = .boldSystemFont(ofSize: 26)
+        lb.textAlignment = .center
+        return lb
+    }()
+    
+    private let scannerButton: UIButton = {
+       let btn = UIButton()
+        btn.setTitle("Scanner", for: .normal)
+        btn.setTitleColor(.black, for: .normal)
+        btn.backgroundColor = .systemOrange
+        btn.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        btn.layer.cornerRadius = 15
+        return btn
+    }()
+    
+    private let controlButton: UIButton = {
+       let btn = UIButton()
+        btn.setTitle("Control", for: .normal)
+        btn.setTitleColor(.black, for: .normal)
+        btn.backgroundColor = .systemOrange
+        btn.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        btn.layer.cornerRadius = 15
+        return btn
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        setLayout()
     }
+
+}
+
+// MARK: - Layout
+extension HomeVC {
     
+    private func setLayout() {
+        [headerLabel,scannerButton, controlButton].forEach {
+            self.view.addSubview($0)
+        }
+        
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        headerLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        headerLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 135).isActive = true
+        
+        scannerButton.translatesAutoresizingMaskIntoConstraints = false
+        scannerButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        scannerButton.topAnchor.constraint(equalTo: self.headerLabel.bottomAnchor, constant: 99).isActive = true
+        scannerButton.widthAnchor.constraint(equalToConstant: 217).isActive = true
+        scannerButton.heightAnchor.constraint(equalToConstant: 43).isActive = true
+        
+        controlButton.translatesAutoresizingMaskIntoConstraints = false
+        controlButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        controlButton.topAnchor.constraint(equalTo: self.scannerButton.bottomAnchor, constant: 94).isActive = true
+        controlButton.widthAnchor.constraint(equalToConstant: 217).isActive = true
+        controlButton.heightAnchor.constraint(equalToConstant: 43).isActive = true
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
-
 }
