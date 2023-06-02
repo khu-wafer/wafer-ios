a37875cdcea44cb7334c1c614e7c8bcc06cc0571
daminoworld <ekadls757@gmail.com>
2023-06-02 19:29:28 +0900

    Add: NetworkConstant

---
+++ khu-wafer/khu-wafer/Network/Base/NetworkConstant.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkConstant.swift
+//  khu-wafer
+//
+//  Created by 김담인 on 2023/06/02.
+//
+
+import Foundation
+
+struct NetworkConstant {
+    static let noTokenHeader = ["Content-Type": "application/json"]
+}
